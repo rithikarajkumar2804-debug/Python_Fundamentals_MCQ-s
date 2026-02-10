4e500622dc2ea0976b87f4c6c53988e612744359
Rithiga R <rithikarajkumar2804@gmail.com>
2026-02-10 10:40:54 +0530

    Add files via upload

---
+++ MCQ_PF.py
@@ -0,0 +1,175 @@
+print("Welcome to the Python MCQ Quiz App\n") 
+score = 0 
+
+print("1. Who developed Python?")
+print("a) Dennis Ritchie")
+print("b) James Gosling")
+print("c) Guido van Rossum")
+print("d) Bjarne Stroustrup") 
+answer = input("Enter your answer (a/b/c/d): ")
+if answer == "c": 
+    score += 1 
+ 
+print("2. Python is a ____ language.")
+print("a) Low-level")
+print("b) Machine-level")
+print("c) High-level") 
+print("d) Assembly")
+answer = input("Enter your answer (a/b/c/d): ") 
+if answer == "c": 
+    score += 1 
+ 
+print("3. Which symbol is used for comments in Python?") 
+print("a) //") 
+print("b) #") 
+print("c) /* */")
+print("d) <!-- -->") 
+answer = input("Enter your answer (a/b/c/d): ") 
+if answer == "b":    
+    score += 1 
+ 
+print("4. Which of the following is a valid variable name?")
+print("a) 2name")
+print("b) my-name")
+print("c) my_name")
+print("d) my name") 
+answer = input("Enter your answer (a/b/c/d): ") 
+if answer == "c":     
+    
+    score += 1 
+ 
+print("5. What is the file extension of Python file?")
+print("a) .java")
+print("b) .py") 
+print("c) .python")
+print("d) .pt") 
+answer = input("Enter your answer (a/b/c/d): ")
+if answer == "b":    
+     score += 1
+     
+print("7.Which data type is used to store text in python?") 
+print("a)int") 
+print("b)float")
+print("c)str") 
+print("d)char") 
+answer = input("Enter your answer (a/b/c/d): ") 
+if answer == "c":     
+    score += 1 
+ 
+print("8.Why  keyword is used to define a function?") 
+print("a)func") 
+print("b)function")
+print("c)def")
+print("d)define")
+answer = input("Enter your answer (a/b/c/d): ") 
+if answer == "c":     
+    score += 1 
+ 
+print("9.Which loop is used to repeat a block of code?")
+print("a)repeat") 
+print("b)while")
+print("c)loop") 
+print("d)until") 
+answer = input("Enter your answer (a/b/c/d): ") 
+if answer == "b":     
+    score += 1 
+ 
+print("10.What is the output of print(type(10))") 
+print("a)int") 
+print("b)number")
+print("c)<class 'int>") 
+print("d)integer") 
+answer = input("Enter your answer (a/b/c/d): ") 
+if answer == "c":     
+    score += 1 
+ 
+print("11.Which data type stores multiple values in order?") 
+print("a)set")
+print("b)dict")
+print("c)list")
+print("d)tuple") 
+answer = input("Enter your answer (a/b/c/d): ")
+if answer == "c":   
+    score += 1 
+ 
+print("12.Which bracket is uesd to create a list?")
+print("a)()")
+print("b){}")
+print("c)[]")
+print("d)<>")
+answer = input("Enter your answer (a/b/c/d): ") 
+if answer == "c":    
+    score += 1 
+ 
+print("13.Which operator checks equality") 
+print("a)=") 
+print("b)==")
+print("c)!=")
+print("d)<=")
+answer = input("Enter your answer (a/b/c/d): ") 
+if answer == "c":    
+    score += 1 
+ 
+print("14.Which of the following is NOT a Python data type?") 
+print("a)list") 
+print("b)tuple")
+print("c)array") 
+print("d)dict") 
+answer = input("Enter your answer (a/b/c/d): ")
+if answer == "c":    
+    score += 1 
+ 
+print("15.Which function returns the length of a list?") 
+print("a)count()") 
+print("b)size()")
+print("c)length()")
+print("d)len()") 
+answer = input("Enter your answer (a/b/c/d): ") 
+if answer == "d":    
+    score += 1 
+ 
+print("16.Which keyword is used to exit a loop?") 
+print("a)stop") 
+print("b)exit") 
+print("c)break")
+print("d)end") 
+answer = input("Enter your answer (a/b/c/d): ") 
+if answer == "c":     
+    score += 1 
+ 
+print("17.Which of the following is used to store key-value pairs?")
+print("a)list") 
+print("b)tuple")
+print("c)set")
+print("d)end")
+answer = input("Enter your answer (a/b/c/d): ")
+if answer == "c":    
+    score += 1 
+ 
+print("18.Which data type is immutable?")
+print("a)list") 
+print("b)dictionary")
+print("c)set") 
+print("d)tuple")
+answer = input("Enter your answer (a/b/c/d): ")
+if answer == "d":     
+    score += 1 
+ 
+print("19.Which loop is best when the number of iterations is known?")
+print("a)while") 
+print("b)for") 
+print("c)do-while")
+print("d)repeat")
+answer = input("Enter your answer (a/b/c/d): ")
+if answer == "b":     
+    score += 1 
+ 
+print("20.Which keyword is used to handle exceptions?")
+print("a)catch") 
+print("b)error")
+print("c)try") 
+print("d)handle") 
+answer = input("Enter your answer (a/b/c/d): ")
+if answer == "b": 
+    score += 1 
+ 
